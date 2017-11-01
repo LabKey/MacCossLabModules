2506a383ac6cd5e70afdb2b1dab0f339d0be8d23
Ed Younskevicius <eyounske@labkey.com>
2017-11-01 02:02:40 +0000

    Update copyrights for 17.3
    SVN r54833 |2017-11-01 02:02:40 +0000

--- panoramapublic/webapp/TargetedMS/js/ExperimentAnnotations.js
+++ panoramapublic/webapp/TargetedMS/js/ExperimentAnnotations.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2017 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 function showShareLink(el, link)
 {
     var imgUrl = LABKEY.contextPath + "/TargetedMS/images/clippy.png";
