25884ad989c99ed5acafc6a4685db064b0168068
Josh Eckels <jeckels@labkey.com>
2018-06-15 21:56:55 +0000

    Delete any lingering iRT scales during container deletion
    SVN r58699 |2018-06-15 21:56:55 +0000

--- PanoramaPublic/src/org/labkey/panoramapublic/TargetedMSListener.java
+++ PanoramaPublic/src/org/labkey/panoramapublic/TargetedMSListener.java
@@ -67,6 +67,9 @@ public class TargetedMSListener implements ExperimentListener, ContainerManager.
         // Clean up Guide Sets
         new SqlExecutor(TargetedMSManager.getSchema()).execute("DELETE FROM " + TargetedMSManager.getTableInfoGuideSet() + " WHERE Container = ?", c);
 
+        // Clean up any orphaned iRT scales
+        TargetedMSManager.deleteiRTscales(c);
+
         // Clean up AutoQCPing
         new SqlExecutor(TargetedMSManager.getSchema()).execute("DELETE FROM " + TargetedMSManager.getTableInfoAutoQCPing() + " WHERE Container = ?", c);
 
