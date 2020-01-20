dd014f1427cf88849ead8f506a7ce8c134e66704
vagisha <vagisha@gmail.com>
2020-01-20 14:59:51 -0800

    LINCS: Removed old code related to generating processed (L3, L4) GCTs on PanoramaWeb. Processed GCTs are being generated on the Broad Institute's server and uploaded to PanoramaWeb. (#24)
    Added a module property to set the assay type for a container.  The assay type is used to decide which R script will be run to generate the L2 GCT on PanoramaWeb, as well as sent as parameter to the Broad server.

--- lincs/resources/reports/schemas/targetedms/GCT_input_peptidearearatio/LabKeyInput.r
+++ lincs/resources/reports/schemas/targetedms/GCT_input_peptidearearatio/LabKeyInput.r
@@ -19,8 +19,6 @@ require(reshape2)
 
 baseUrl <- function(){
   return(paste(labkey.url.base));
-  # return(paste("https://panoramaweb.org:8443/labkey/"));
-  # return (paste("http://localhost:8080/labkey"));
 }
 
 selectedReplicateIds <- vector('numeric');
