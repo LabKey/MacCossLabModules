3efd0779f966aa9f9a73d87c0485bc17b83e903c
vagisha <vagisha@gmail.com>
2021-03-06 08:23:54 -0800

    Chrom lib support

--- panoramapublic/src/org/labkey/panoramapublic/PanoramaPublicManager.java
+++ panoramapublic/src/org/labkey/panoramapublic/PanoramaPublicManager.java
@@ -16,18 +16,28 @@
 
 package org.labkey.panoramapublic;
 
+import org.apache.commons.io.FileUtils;
 import org.labkey.api.data.Container;
 import org.labkey.api.data.DbSchema;
+import org.labkey.api.data.PropertyManager;
 import org.labkey.api.data.TableInfo;
 import org.labkey.api.module.Module;
 import org.labkey.api.module.ModuleLoader;
 import org.labkey.api.module.ModuleProperty;
+import org.labkey.api.pipeline.PipeRoot;
+import org.labkey.api.pipeline.PipelineService;
 import org.labkey.api.portal.ProjectUrls;
 import org.labkey.api.security.User;
 import org.labkey.api.targetedms.ITargetedMSRun;
 import org.labkey.api.targetedms.TargetedMSService;
 import org.labkey.api.util.PageFlowUtil;
 import org.labkey.api.view.ActionURL;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PanoramaPublicManager
 {
@@ -78,13 +88,83 @@ public class PanoramaPublicManager
         return PageFlowUtil.urlProvider(ProjectUrls.class).getBeginURL(container, TargetedMSService.RAW_FILES_TAB);
     }
 
-    public static void makePanoramaExperimentalDataFolder(Container container, User user)
+    public static void makePanoramaExperimentalDataFolder(Container container, Container sourceContainer, User user)
     {
         Module targetedMSModule = ModuleLoader.getInstance().getModule(TargetedMSService.MODULE_NAME);
-        ModuleProperty moduleProperty = targetedMSModule.getModuleProperties().get(TargetedMSService.FOLDER_TYPE_PROP_NAME);
         if(container.getActiveModules().contains(targetedMSModule))
         {
-            moduleProperty.saveValue(user, container, TargetedMSService.FolderType.Experiment.toString());
+            ModuleProperty moduleProperty = targetedMSModule.getModuleProperties().get(TargetedMSService.FOLDER_TYPE_PROP_NAME);
+            String sourceValue = moduleProperty.getValueContainerSpecific(sourceContainer);
+            String targetValue = moduleProperty.getValueContainerSpecific(container);
+            if(TargetedMSService.FolderType.Library.name().equals(sourceValue) || TargetedMSService.FolderType.LibraryProtein.name().equals(sourceValue))
+            {
+                PropertyManager.PropertyMap propMap = PropertyManager.getProperties(sourceContainer, "TargetedMS");
+                String versionStr = propMap.get("chromLibRevision");
+                if(null != versionStr)
+                {
+                    // int version = Integer.parseInt(versionStr);
+                    propMap = PropertyManager.getWritableProperties(container, "TargetedMS", true);
+                    propMap.put("chromLibRevision", versionStr);
+                    propMap.save();
+
+                    Path chromLibDir = getChromLibDir(container);
+                    if(Files.exists(chromLibDir))
+                    {
+                        try
+                        {
+                            Files.list(chromLibDir).forEach(p -> {
+                                try
+                                {
+                                    changeFileName(p, container);
+                                }
+                                catch (IOException e)
+                                {
+                                    e.printStackTrace();
+                                }
+                            });
+                        }
+                        catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+
+            // moduleProperty.saveValue(user, container, TargetedMSService.FolderType.Experiment.toString());
         }
+    }
+
+    public static final String LIB_FILE_DIR = "targetedMSLib";
+    public static final String CHROM_LIB_FILE_NAME = "chromlib";
+    public static final String CHROM_LIB_FILE_EXT = "clib";
+    // Example: chromlib_314_rev1.clib
+    public static Pattern chromlibPattern = Pattern.compile(CHROM_LIB_FILE_NAME + "_\\d+_rev(\\d+)\\." + CHROM_LIB_FILE_EXT);
+    private static void changeFileName(Path path, Container container) throws IOException
+    {
+        if(path.getFileName().toString().endsWith(CHROM_LIB_FILE_EXT))
+        {
+            Matcher match = chromlibPattern.matcher(path.getFileName().toString());
+            if(match.matches())
+            {
+                String revision = match.group(1);
+                String newFileName = CHROM_LIB_FILE_NAME + "_"+container.getRowId() + "_rev"+revision + "." + CHROM_LIB_FILE_EXT;
+                Path targetFile = path.getParent().resolve(newFileName);
+                FileUtils.moveFile(path.toFile(), targetFile.toFile());
+            }
+        }
+    }
+
+    public static Path getChromLibFile(Container container, int revision) throws IOException
+    {
+        Path chromLibDir = getChromLibDir(container);
+        return chromLibDir.resolve(CHROM_LIB_FILE_NAME+"_"+container.getRowId()+"_rev"+revision+"."+CHROM_LIB_FILE_EXT);
+    }
+
+    private static Path getChromLibDir(Container container)
+    {
+        PipeRoot root = PipelineService.get().getPipelineRootSetting(container);
+        assert root != null;
+        return root.getRootNioPath().resolve(LIB_FILE_DIR);
     }
 }
