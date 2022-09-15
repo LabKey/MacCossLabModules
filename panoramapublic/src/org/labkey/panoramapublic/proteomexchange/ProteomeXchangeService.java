d6a9b82381363d54f07e61badce1d7a5f223793f
Adam Rauch <adam@labkey.com>
2022-09-15 14:30:10 -0700

    Restore important import (thanks for removing it, IntelliJ)

--- panoramapublic/src/org/labkey/panoramapublic/proteomexchange/ProteomeXchangeService.java
+++ panoramapublic/src/org/labkey/panoramapublic/proteomexchange/ProteomeXchangeService.java
@@ -15,7 +15,6 @@
  */
 package org.labkey.panoramapublic.proteomexchange;
 
-
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.entity.mime.FileBody;
 import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
