ee15ffc88e64a75129116a79eccd87b9d85ae2ae
ankurjuneja <ankurjuneja05@gmail.com>
2024-10-30 16:47:27 -0700

    Nextflow integration with pipeline

---
+++ nextflow/src/org/labkey/nextflow/pipeline/NextFlowPipelineJob.java
@@ -0,0 +1,60 @@
+package org.labkey.nextflow.pipeline;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.labkey.api.pipeline.PipeRoot;
+import org.labkey.api.pipeline.PipelineJob;
+import org.labkey.api.pipeline.PipelineJobService;
+import org.labkey.api.pipeline.TaskId;
+import org.labkey.api.pipeline.TaskPipeline;
+import org.labkey.api.util.FileUtil;
+import org.labkey.api.util.URLHelper;
+import org.labkey.api.util.UnexpectedException;
+import org.labkey.api.view.ViewBackgroundInfo;
+
+import java.io.IOException;
+
+public class NextFlowPipelineJob extends PipelineJob
+{
+    private String _apiKey;
+    // For serialization
+    protected NextFlowPipelineJob()
+    {}
+
+    public NextFlowPipelineJob(ViewBackgroundInfo info, @NotNull PipeRoot root, String apiKey)
+    {
+        super(null, info, root);
+        this._apiKey = apiKey;
+        try
+        {
+            setLogFile(FileUtil.createTempFile("NextFlowPipelineJob", ".log"));
+        }
+        catch (IOException e)
+        {
+            throw new UnexpectedException(e);
+        }
+    }
+
+    public String getApiKey()
+    {
+        return _apiKey;
+    }
+
+    @Override
+    public URLHelper getStatusHref()
+    {
+        return null;
+    }
+
+    @Override
+    public String getDescription()
+    {
+        return "NextFlow Job";
+    }
+
+    @Override
+    public TaskPipeline getTaskPipeline()
+    {
+        return PipelineJobService.get().getTaskPipeline(new TaskId(NextFlowPipelineJob.class));
+    }
+}
